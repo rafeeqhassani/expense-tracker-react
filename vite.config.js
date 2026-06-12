304556f453a1e6cf409645c857bb6449e98e2508
rafiqhassani <rafiqhassani106@gmail.com>
2026-06-12 09:23:03 +0500

    setup auto deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/expense-tracker-react/",
   plugins: [react()],
+  base: "/expense-tracker-react/",
 });
