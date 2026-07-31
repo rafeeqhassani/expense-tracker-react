9d98a342d665a02ebe9cf6912b6438469fb1d2ec
rafiqhassani <rafiqhassani106@gmail.com>
2026-07-31 11:10:21 +0500

    improve local api proxy server error handling

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,22 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000",
         changeOrigin: true,
+
+        configure(proxy) {
+          proxy.on("error", (error, req, res) => {
+            console.log("Backend server is unavailable");
+
+            res.writeHead(503, {
+              "Content-Type": "application/json",
+            });
+
+            res.end(
+              JSON.stringify({
+                message: "Unable to connect to the server.",
+              }),
+            );
+          });
+        },
       },
     },
   },
